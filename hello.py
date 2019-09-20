555fd1fb01222d19afc2ee323e59b973613ee0b2
Onetechsup <jungleminus@gmail.com>
2019-09-21 00:56:13 +0300

    Revert "Update hello.py"
    This reverts commit 58a6a6996d6bd4de5b1c4c6ae7d9c5a22a657b97.

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 def application(environ, start_response):
 	body = "\n".join(environ['QUERY_STRING'].split('&'))
-#	body = bytes(body, encoding='utf-8')
+	body = bytes(body, encoding='utf-8')
 	status = '200 OK'
 	headers = [('Content-Type', 'text/plain')]
 	start_response(status, headers)
