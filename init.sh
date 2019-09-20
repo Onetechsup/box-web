fba36bbf47fac42d8129cc3e06265717ba6a7026
Onetechsup <jungleminus@gmail.com>
2019-09-21 00:17:22 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -6,10 +6,10 @@ sudo /etc/init.d/nginx restart
 #sudo nginx -c /etc/nginx/sites-enabled/test.conf
 
 # settings for gunicorn
-sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
+sudo ln -s /home/box/web/etc/gunicorn.py /etc/gunicorn.d/hello.py
 cd /home/box/web
 #sudo gunicorn -b 0.0.0.0:8080 hello:application
-sudo gunicorn -c /etc/gunicorn.d/test hello:application
+sudo gunicorn -c /etc/gunicorn.d/hello.py hello:application
 
 # settings for mysql
 #sudo /etc/init.d/mysql start
